b56039b457acfe97eead63b0776e4fe98b89b9ab
Yevhenii <jenmikitenko@gmail.com>
2024-09-22 15:02:59 +0300

    add current socket server

--- client/src/socket-io.tsx
+++ client/src/socket-io.tsx
@@ -3,6 +3,6 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 //const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
 
-export const socket = io('http://localhost:3000',{
+export const socket = io('https://socket-app-7knc.onrender.com/',{
     autoConnect: false
 });
